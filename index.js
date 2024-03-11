2aa573009fb40dc2cdbd9f68b1219514e1c9deb8
lili <162986028+xlilix-png@users.noreply.github.com>
2024-03-11 19:14:59 +0800

    index.js

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    activities: [{ name: currentStatus, type: ActivityType.Streaming, url:
+      "https://www.twitch.tv/blitzz_seal" }],
     status: 'dnd',
   });
 
